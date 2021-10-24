23dd98f3d6c0e9a684bd9d70d17ebde7edae519f
iLikeTrioxin <iLike135Trioxane@protonmail.com>
2021-10-24 23:31:39 +0200

    .

--- Hei/Source/Main.cpp
+++ Hei/Source/Main.cpp
@@ -1,14 +1,16 @@
-#include "Core/Files.h"
-#include "Components.h"
-#include "Renderer/Renderer.h"
-#include "modelLoader.h"
-#include "DebugTools.h"
-#include "Renderer/Shader.h"
-#include "Renderer/Text.h"
-#include "Scene.h"
-#include "./Core/Window.h"
-#include "Entity.h"
+#define PETROL_ENGINE_DEBUG
+#include <Core/Files.h>
+#include <Components.h>
+#include <Renderer/Renderer.h>
+#include <modelLoader.h>
+#include <DebugTools.h>
+#include <Renderer/Shader.h>
+#include <Renderer/Text.h>
+#include <Scene.h>
+#include <Core/Window.h>
+#include <Entity.h>
 #include <GLFW/glfw3.h>
+#include <Utils/Benchmarker.h>
 
 using namespace PetrolEngine;
 
@@ -31,7 +33,7 @@ Game::Game() {
     DEBUG_LOG("Size of int32 is " << sizeof(int32));
     DEBUG_LOG("Size of int64 is " << sizeof(int64));
 
-    this->window = Window::create(800, 450, "Hei");
+    this->window = Window::create(800, 600, "Hei");
 
     Image::flipImages(true);
 
@@ -49,11 +51,11 @@ Game::Game() {
     basic->setInt  ("material.diffuse"  , 0  );
     basic->setInt  ("material.specular" , 0  );
     basic->setFloat("material.shininess", 1. );
-
     basic->setInt  ("light[0].lightType", 1  );
-    basic->setVec3 ("light[0].direction", -1.0f, 0.0f, 1.0f);
+
+    basic->setVec3 ("light[0].direction", 1.0f, 0.0f, 1.0f);
     basic->setVec3 ("light[0].ambient"  , 0.2f, 0.2f, 0.2f);
-    basic->setVec3 ("light[0].diffuse"  , glm::vec3(3.0f, 3.0f, 3.0f));
+    basic->setVec3 ("light[0].diffuse"  , 3.0f, 3.0f, 3.0f);
     basic->setVec3 ("light[0].specular" , 0.0f, 0.0f, 0.0f);
 
     ModelLoader::Get().shader = basic;
@@ -85,17 +87,6 @@ void Game::gameLoop() { LOG_FUNCTION();
 
     camera.updatePerspectiveMatrix(window->getAspectRatio());
 
-    std::vector<double> deltaTimes;
-    deltaTimes.reserve(100);
-
-    struct sortDouble {
-        bool operator()(double a, double b) const { return a < b; }
-    };
-
-    double dotOnePercentLow = 0.1;
-    double    OnePercentLow = 0.1;
-    double average          = 0.1;
-
     uint idk = 0;
     Renderer::getDeviceConstantValue(DeviceConstant::MAX_TEXTURE_IMAGE_UNITS, (void*) &idk);
     DEBUG_LOG("aaa:"<<std::to_string(idk));
@@ -103,6 +94,8 @@ void Game::gameLoop() { LOG_FUNCTION();
     double previousXCursorPos = cursorXPos;
     double previousYCursorPos = cursorYPos;
     double previousFrame      = glfwGetTime();
+
+    Benchmarker benchmarker = Benchmarker( (void*) glfwGetTime );
 
     while (!window->shouldClose())
     {
@@ -116,27 +109,6 @@ void Game::gameLoop() { LOG_FUNCTION();
         deltaTime     = currentFrame - previousFrame;
         previousFrame = currentFrame;
 
-        uint32_t frameCount = deltaTimes.size();
-        if (frameCount >= 100) {
-            std::sort(deltaTimes.begin(), deltaTimes.end(), sortDouble());
-            dotOnePercentLow = (deltaTimes.back() + dotOnePercentLow) * 0.5;
-
-            double sum = 0.0;
-            for (uint32_t i = 0; i < 10; i++) { sum += deltaTimes[frameCount - i - 1]; }
-            OnePercentLow = (OnePercentLow + (sum * 0.1)) * 0.5;
-
-            sum = 0.0;
-            for (auto& e : deltaTimes) sum += e;
-
-            average = sum * 0.01;
-
-            deltaTimes.clear();
-        }
-
-        deltaTimes.push_back(deltaTime);
-
-        previousFrame = glfwGetTime();
-
         LOG_SCOPE("Frame");
         Renderer::clear();
 
@@ -147,15 +119,15 @@ void Game::gameLoop() { LOG_FUNCTION();
             auto shad1 = ReadFile("../Hei/Resources/Shaders/shader.vert");
             auto shad2 = ReadFile("../Hei/Resources/Shaders/shader.frag");
 
-            Shader::load("default", "")->recompileShader(shad1.c_str(), shad2.c_str());
+            Shader::load("default", "")->recompileShader(shad1.c_str(), shad2.c_str() );
         }
 
-        auto* wr = EventStack::getEvents<Window::WindowResizedEvent>();
-        if (wr != nullptr) {
-            for (auto w : *wr) {
-                camera.updatePerspectiveMatrix(window->getAspectRatio());
-                Renderer::OnWindowResize(w->data.width, w->data.height);
-            }
+        auto& wr = EventStack::getEvents<Window::WindowResizedEvent>();
+        for (auto* w : wr) {
+            camera.updatePerspectiveMatrix(window->getAspectRatio());
+            Renderer::OnWindowResize(w->data.width, w->data.height);
+            wr.erase(wr.begin());
+            DEBUG_LOG(benchmarker.getMin());
         }
 
         camMov.update(window, mainCamera);
@@ -182,6 +154,7 @@ void Game::gameLoop() { LOG_FUNCTION();
 
         window->swapBuffers();
         window->pollEvents ();
+        benchmarker.frameDone();
     }
 }
 
